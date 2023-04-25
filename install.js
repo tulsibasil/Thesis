e64a39c6c8d7b6ddb59142cc3dc65b6a74243efa
tulsibasil <tulsi.patel@yale.edu>
2023-04-25 19:24:02 -0400

    i luv coding

---
+++ install.js
@@ -0,0 +1,75 @@
+function openani(){
+	window.open("animation.html")
+}
+
+function openpent(){
+	window.open("sculpture.html")
+}
+
+function openweb(){
+	window.open("https://tulsibasil.github.io/surveillance/surveillance.html")
+}
+
+
+
+
+
+const ani = document.querySelector("#ani");
+const data = document.querySelector("#data");
+
+ani.addEventListener("mouseover", function() {
+  data.style.color = "black"; // Change the color of #data to red
+});
+ani.addEventListener("mouseout", function() {
+  data.style.color = ""; // Reset the color of #data to its original value
+});
+
+data.addEventListener("mouseover", function() {
+  ani.style.transform = "scale(1.2)"; // Scale up #ani by 1.2
+});
+
+data.addEventListener("mouseout", function() {
+  ani.style.transform = ""; // Reset the scale of #ani to its original value
+});
+
+
+const web = document.querySelector("#web");
+const inter = document.querySelector("#inter");
+
+web.addEventListener("mouseover", function() {
+  inter.style.color = "black"; // Change the color of #data to red
+});
+web.addEventListener("mouseout", function() {
+  inter.style.color = ""; // Reset the color of #data to its original value
+});
+
+inter.addEventListener("mouseover", function() {
+  web.style.transform = "scale(1.2)"; // Scale up #ani by 1.2
+});
+
+inter.addEventListener("mouseout", function() {
+  web.style.transform = ""; // Reset the scale of #ani to its original value
+});
+
+
+
+
+const pent = document.querySelector("#pent");
+const invert = document.querySelector("#invert");
+
+pent.addEventListener("mouseover", function() {
+  invert.style.color = "black"; // Change the color of #data to red
+});
+pent.addEventListener("mouseout", function() {
+  invert.style.color = ""; // Reset the color of #data to its original value
+});
+
+invert.addEventListener("mouseover", function() {
+  pent.style.transform = "scale(1.2)"; // Scale up #ani by 1.2
+});
+
+invert.addEventListener("mouseout", function() {
+  pent.style.transform = ""; // Reset the scale of #ani to its original value
+});
+
+
